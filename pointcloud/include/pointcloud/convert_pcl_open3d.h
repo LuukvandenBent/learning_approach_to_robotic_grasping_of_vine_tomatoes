7e7c7a0e8c3ae4ec614d4bb637882d070caaa053
LuukvandenBent <luukbent@gmail.com>
2023-06-28 16:39:28 +0200

    Remove open3d dependecies since they are unused

--- pointcloud/include/pointcloud/convert_pcl_open3d.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <open3d/Open3D.h>
-#include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
-
-pcl::PointCloud<pcl::PointXYZRGB>::Ptr open3d_to_pcl(const std::shared_ptr<open3d::geometry::PointCloud> open3d_cloud);
-std::shared_ptr<open3d::geometry::PointCloud> pcl_to_open3d( const pcl::PointCloud<pcl::PointXYZRGB>::Ptr pcl_cloud);
